d4805c64313cc2094401e7707c25ebbc1f1dceb9
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2023-05-10 17:08:06 +0100

    com

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useContext, useState, useEffect } from "react";
 import { onAuthStateChanged, signOut as authSignOut } from "firebase/auth";
 import { auth } from "./firebase";
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,13 +3,20 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBC6fgVwFz-PUsosoveTW1sgof_xqWNZes",
-    authDomain: "todo-app-f769a.firebaseapp.com",
-    projectId: "todo-app-f769a",
-    storageBucket: "todo-app-f769a.appspot.com",
-    messagingSenderId: "127294715055",
-    appId: "1:127294715055:web:d4a9149bef3e243784105a",
-    measurementId: "G-Q79Z1PGGC6",
+    // apiKey: "AIzaSyBC6fgVwFz-PUsosoveTW1sgof_xqWNZes",
+    // authDomain: "todo-app-f769a.firebaseapp.com",
+    // projectId: "todo-app-f769a",
+    // storageBucket: "todo-app-f769a.appspot.com",
+    // messagingSenderId: "127294715055",
+    // appId: "1:127294715055:web:d4a9149bef3e243784105a",
+    // measurementId: "G-Q79Z1PGGC6",
+    apiKey: "AIzaSyCcGY-9_DceFJCem2c_P40F3XwLnxMxPkA",
+    authDomain: "annuaire-app-ef37a.firebaseapp.com",
+    projectId: "annuaire-app-ef37a",
+    storageBucket: "annuaire-app-ef37a.appspot.com",
+    messagingSenderId: "584438830748",
+    appId: "1:584438830748:web:34652cb5dee29190f5bf59"
+    
 };
 
 const app = initializeApp(firebaseConfig);
